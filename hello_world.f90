95dc9304e202786ccc9fe50a3a5349b79af6c635
Sam Harrison <samharrison.xg@gmail.com>
2023-02-28 11:47:53 +0000

    Adding date

--- hello_world.f90
+++ hello_world.f90
@@ -1,5 +1,10 @@
 program hello_world
     implicit none
 
+    character(8) :: date
+
+    call date_and_time(date=date)
+
     print *, "Hello world!"
+    print *, "The current date is ", date
 end program
