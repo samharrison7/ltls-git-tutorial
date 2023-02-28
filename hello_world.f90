fee59dafa53a3159d18f94203d2188d60790857a
Sam Harrison <samharrison.xg@gmail.com>
2023-02-28 12:23:45 +0000

    Adding code comments

--- hello_world.f90
+++ hello_world.f90
@@ -1,11 +1,14 @@
 program hello_world
     implicit none
 
+    ! Declare vars for date and time
     character(8) :: date
     character(10) :: time
 
+    ! Get the current date and time
     call date_and_time(date=date, time=time)
 
+    ! Print the output
     print *, "Hello world!"
     print *, "The current date is ", date
     print *, "The current time is ", time
